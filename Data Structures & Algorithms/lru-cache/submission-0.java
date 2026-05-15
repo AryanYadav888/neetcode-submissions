4ad8553ecd40582997c3d8a426df7a9981605563
AryanYadav888 <aryanceo888@gmail.com>
2026-05-16 01:43:01 +0530

    Add: lru-cache - submission-0

---
+++ Data Structures & Algorithms/lru-cache/submission-0.java
@@ -0,0 +1,42 @@
+class LRUCache {
+
+    private List<int[]> cache;
+    private int capacity;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        cache = new ArrayList<>();
+    }
+
+    public int get(int key) {
+
+        for (int i = 0; i < cache.size(); i++) {
+            if (cache.get(i)[0] == key) {
+                int value = cache.get(i)[1];
+                int[] temp = cache.get(i);
+                cache.remove(i);
+                cache.add(temp);
+                return value;
+            }
+        }
+        return -1;
+    }
+
+    public void put(int key, int value) {
+
+        for (int i = 0; i < cache.size(); i++) {
+            if (cache.get(i)[0] == key) {
+                cache.remove(i);
+                cache.add(new int[]{key, value});
+                return;
+            }
+        }
+
+        if (cache.size() == capacity) {
+            cache.remove(0);
+            cache.add(new int[]{key, value});
+        } else {
+            cache.add(new int[]{key, value});
+        }
+    }
+}
